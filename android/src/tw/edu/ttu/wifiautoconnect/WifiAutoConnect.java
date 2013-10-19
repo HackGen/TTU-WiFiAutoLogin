816b67c27e75aee8432a8eaa091d31995f0af648
Denny Huang <denny0223@gmail.com>
2013-10-20 04:33:13 +0800

    remove option menu

--- android/src/tw/edu/ttu/wifiautoconnect/WifiAutoConnect.java
+++ android/src/tw/edu/ttu/wifiautoconnect/WifiAutoConnect.java
@@ -3,7 +3,6 @@ package tw.edu.ttu.wifiautoconnect;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 
 public class WifiAutoConnect extends Activity {
 
@@ -11,13 +10,6 @@ public class WifiAutoConnect extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi_auto_connect);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.wifi_auto_connect, menu);
-        return true;
     }
 
 }
