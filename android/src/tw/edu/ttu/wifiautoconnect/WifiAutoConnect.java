49adbed283bdcd6d75538cc9411b6d412cb65af1
Denny Huang <denny0223@gmail.com>
2013-10-20 05:01:55 +0800

    add onclick method, and show passwoed OK!

--- android/src/tw/edu/ttu/wifiautoconnect/WifiAutoConnect.java
+++ android/src/tw/edu/ttu/wifiautoconnect/WifiAutoConnect.java
@@ -2,14 +2,40 @@
 package tw.edu.ttu.wifiautoconnect;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.app.Activity;
 
 public class WifiAutoConnect extends Activity {
+    EditText usernameEditText;
+    EditText passwordEditText;
+    CheckBox showPasswordCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi_auto_connect);
+
+        usernameEditText = (EditText) findViewById(R.id.et_username);
+        passwordEditText = (EditText) findViewById(R.id.et_password);
+        showPasswordCheckBox = (CheckBox) findViewById(R.id.cb_showpwd);
+    }
+
+    public void showPasswordCheckBoxOnclick(View view) {
+        if (showPasswordCheckBox.isChecked()) {
+            passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT
+                    | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+        }
+        else {
+            passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT
+                    | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        }
+    }
+
+    public void loginBthOnclick(View view) {
+
     }
 
 }
