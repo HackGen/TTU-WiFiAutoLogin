c777014f54a09d6a34944e3a75ab69523e8916a1
Denny Huang <denny0223@gmail.com>
2013-10-19 15:58:37 +0800

    remove debug message, check and show login result

--- unix-like/login.sh
+++ unix-like/login.sh
@@ -1,3 +1,9 @@
 source ./config.sh
 
-curl -i -d "user=$user&password=$password&cmd=authenticate&Login=Log+In" $url
+result=$(curl -i -d "user=$user&password=$password&cmd=authenticate&Login=Log+In" $url 2> /dev/null | grep 'User Authenticated' | wc -l)
+
+if [ $result -eq 1 ]; then
+	echo "Success!!";
+else
+	echo "Something wrong!";
+fi
