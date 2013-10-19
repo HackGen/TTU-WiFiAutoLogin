97042381f69dcbf06ee04aade5399f4a8e19cff6
Denny Huang <denny0223@gmail.com>
2013-10-20 07:57:28 +0800

    Login without check result ok!

--- android/src/tw/edu/ttu/wifiautoconnect/WifiAutoConnect.java
+++ android/src/tw/edu/ttu/wifiautoconnect/WifiAutoConnect.java
@@ -1,9 +1,21 @@
 
 package tw.edu.ttu.wifiautoconnect;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import tw.edu.ttu.network.NetworkStatus;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -11,6 +23,7 @@ import android.widget.Toast;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 
 public class WifiAutoConnect extends Activity {
     public static final String PREF = "ACCOUNT_PREF";
@@ -50,6 +63,51 @@ public class WifiAutoConnect extends Activity {
     }
 
     public void loginBthOnclick(View view) {
+        final ProgressDialog progressDialog;
+
+        progressDialog = ProgressDialog.show(this, "Title", "Message");
+
+        new Thread() {
+            @Override
+            public void run() {
+                Resources resources = getResources();
+
+                String url = getString(R.string.url);
+                String[] params_name = resources.getStringArray(R.array.params_name);
+                String[] params_value = resources.getStringArray(R.array.params_value);
+                params_value[0] = usernameEditText.getText().toString();
+                params_value[1] = passwordEditText.getText().toString();
+
+                if (params_name.length != params_value.length)
+                    Log.e(getString(R.string.app_name), "Config file error!");
+
+                List<BasicNameValuePair> parms = new LinkedList<BasicNameValuePair>();
+
+                for (int i = 0; i < params_name.length; i++) {
+                    Log.d("asd", params_name[i] + " " + params_value[i]);
+                    parms.add(new BasicNameValuePair(params_name[i], params_value[i]));
+                }
+
+                try {
+                    HttpPost httpPost = new HttpPost(url);
+                    httpPost.setEntity(new UrlEncodedFormEntity(parms, "UTF-8"));
+                    new DefaultHttpClient().execute(httpPost);
+
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (ClientProtocolException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+                progressDialog.dismiss();
+                finish();
+
+            };
+        }.start();
     }
 
     private void restorePrefs() {
