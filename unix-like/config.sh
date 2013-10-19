66081d593e4c7c7d7e912112feaea8a51ad73716
Denny Huang <denny0223@gmail.com>
2013-10-20 03:21:13 +0800

    use https

--- unix-like/config.sh
+++ unix-like/config.sh
@@ -2,5 +2,5 @@ user=
 password=
 
 # DEVELOPER ONLY, DO NOT MODIFY THE FOLLOWING CODE!!
-url=http://securelogin.arubanetworks.com/cgi-bin/login
+url=https://securelogin.arubanetworks.com/cgi-bin/login
 params="user=$user&password=$password&cmd=authenticate&Login=Log+In"
