8ec59689aefe7f5ba3b3c87369daecb7995847fd
Denny Huang <denny0223@gmail.com>
2013-11-22 14:52:58 +0800

    curl enable insecure

--- unix-like/login.sh
+++ unix-like/login.sh
@@ -1,6 +1,6 @@
 source ./config.sh
 
-result=$(curl -i -d "user=$user&password=$password&cmd=authenticate&Login=Log+In" $url 2> /dev/null | grep 'User Authenticated' | wc -l)
+result=$(curl -k -i -d "user=$user&password=$password&cmd=authenticate&Login=Log+In" $url 2> /dev/null | grep 'User Authenticated' | wc -l)
 
 if [ $result -eq 1 ]; then
 	echo "Success!!";
