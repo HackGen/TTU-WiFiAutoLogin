29bffae7b48d986604d9f0d4e3432edf23e45ac4
Denny Huang <denny0223@gmail.com>
2014-01-04 11:45:05 +0800

    Fix source config.sh path problem when symbolic link

--- unix-like/login.sh
+++ unix-like/login.sh
@@ -1,5 +1,5 @@
-CUR_DIR=$(dirname $0)
-source $CUR_DIR/config.sh
+SCRIPT_PATH=$(dirname $(readlink -f $0))
+source $SCRIPT_PATH/config.sh
 
 result=$(curl -k -i -d "user=$user&password=$password&cmd=authenticate&Login=Log+In" $url 2> /dev/null | grep 'User Authenticated' | wc -l)
 
