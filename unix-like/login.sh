43f4068e0b47c1e4ab0870e11d30524265c98703
Denny Huang <denny0223@gmail.com>
2014-01-04 11:24:12 +0800

    Fix source config.sh path problem

--- unix-like/login.sh
+++ unix-like/login.sh
@@ -1,4 +1,5 @@
-source ./config.sh
+CUR_DIR=$(dirname $0)
+source $CUR_DIR/config.sh
 
 result=$(curl -k -i -d "user=$user&password=$password&cmd=authenticate&Login=Log+In" $url 2> /dev/null | grep 'User Authenticated' | wc -l)
 
